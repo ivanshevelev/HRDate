b352298f3409aa50d2d7c15fd1aff0d0236b6ca9
Ivan Shevelev <vanujkee@icloud.com>
2016-03-29 14:23:37 +0600

    add NSCalendar UTCCalendar category

---
+++ Example/HRDate/NSCalendar+HRUTCCalendar.h
@@ -0,0 +1,17 @@
+//
+//  NSCalendar+HRUTCCalendar.h
+//  HRDate
+//
+//  Created by Ivan Shevelev on 29/03/16.
+//  Copyright © 2016 Ivan Shevelev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * NSCalendarHRUTCCalendarUTCKey;
+
+@interface NSCalendar (HRUTCCalendar)
+
++(instancetype)hrUTCCalendar;
+
+@end
