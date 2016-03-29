149836c0d478d02a5a4134e108a302d486c68cf8
Ivan Shevelev <vanujkee@icloud.com>
2016-03-29 15:19:07 +0600

    start writing NSDate category

---
+++ Example/HRDate/NSDate+HRDate.h
@@ -0,0 +1,49 @@
+//
+//  NSDate+HRDate.h
+//  HRDate
+//
+//  Created by Ivan Shevelev on 29/03/16.
+//  Copyright © 2016 Ivan Shevelev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSUInteger const HRDateHoursInDay;
+
+extern NSUInteger const HRDateMinutesInHour;
+extern NSUInteger const HRDateMinutesInDay;
+
+extern NSUInteger const HRDateSecondsInMinute;
+extern NSUInteger const HRDateSecondsInHour;
+extern NSUInteger const HRDateSecondsInDay;
+
+@interface NSDate (HRDateCompare)
+
+-(BOOL)hrIsToday;
+
+@end
+
+@interface NSDate (HRDateComponents)
+
+@property (nonatomic, readonly) NSUInteger hrDay;
+@property (nonatomic, readonly) NSUInteger hrMonth;
+@property (nonatomic, readonly) NSUInteger year;
+
+@end
+
+@interface NSDate (HRDateFactory)
+
+-(nonnull NSDate *)hrDateAfterDays:(NSInteger)days;
+-(nonnull NSDate *)hrYesterday;
+-(nonnull NSDate *)hrTomorrow;
+
+@end
+
+@interface NSDate (HRDateString)
+
+-(nullable NSString *)hrDateStringWithDateFormat:(nonnull NSString *)dateFormat;
+
+-(nonnull NSString *)hrDateStringWithDateStyle:(NSDateFormatterStyle)dateStyle
+                                  andTimeStyle:(NSDateFormatterStyle)timeStyle;
+
+@end
---
+++ Example/HRDate/NSDate+HRDate.m
@@ -0,0 +1,56 @@
+//
+//  NSDate+HRDate.m
+//  HRDate
+//
+//  Created by Ivan Shevelev on 29/03/16.
+//  Copyright © 2016 Ivan Shevelev. All rights reserved.
+//
+
+#import "NSDate+HRDate.h"
+#import "NSDateComponents+HRAllComponents.h"
+
+NSUInteger const HRDateHoursInDay = 24;
+
+NSUInteger const HRDateMinutesInHour = 60;
+NSUInteger const HRDateMinutesInDay = 1440;
+
+NSUInteger const HRDateSecondsInMinute = 60;
+NSUInteger const HRDateSecondsInHour = 3600;
+NSUInteger const HRDateSecondsInDay = 86400;
+
+@implementation NSDate (HRDateCompare)
+
+-(BOOL)hrIsToday {
+    NSDateComponents *selfDateComponenets = [NSDateComponents hrUTCAllComponentsFromDate:self];
+    NSDateComponents *todayDateComponenets = [NSDateComponents hrUTCAllComponentsFromDate:[NSDate date]];
+    
+    return
+    selfDateComponenets.year  == todayDateComponenets.year  &&
+    selfDateComponenets.month == todayDateComponenets.month &&
+    selfDateComponenets.day   == todayDateComponenets.day;
+}
+
+@end
+
+@implementation NSDate (HRDateComponents)
+@end
+
+@implementation NSDate (HRDateFactory)
+@end
+
+@implementation NSDate (HRDateString)
+
+-(nullable NSString *)hrDateStringWithDateFormat:(nonnull NSString *)dateFormat {
+    return nil;
+}
+
+-(nonnull NSString *)hrDateStringWithDateStyle:(NSDateFormatterStyle)dateStyle
+                                  andTimeStyle:(NSDateFormatterStyle)timeStyle {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateStyle = dateStyle;
+    dateFormatter.timeStyle = timeStyle;
+    NSString *dateString = [dateFormatter stringFromDate:self];
+    return dateString;
+}
+
+@end
