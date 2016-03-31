e1a916aca8e22098373735041da132cc9975b7bf
Ivan Shevelev <vanujkee@icloud.com>
2016-03-31 18:47:53 +0600

    add header file

---
+++ Example/HRDate/HRDate.h
@@ -0,0 +1,18 @@
+//
+//  HRDate.h
+//  HRDate
+//
+//  Created by Ivan Shevelev on 31/03/16.
+//  Copyright © 2016 Ivan Shevelev. All rights reserved.
+//
+
+#ifndef HRDate_h
+#define HRDate_h
+
+#import <HRDate/NSDate+HRDate.h>
+#import <HRDate/NSTimeZone+HRUTCTimeZone.h>
+#import <HRDate/NSDateFormatter+HRUTCDateFormatter.h>
+#import <HRDate/NSDateComponents+HRAllComponents.h>
+#import <HRDate/NSCalendar+HRUTCCalendar.h>
+
+#endif /* HRDate_h */
